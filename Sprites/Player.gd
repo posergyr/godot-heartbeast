dac072c6c5adfa20d75cfb357801426bb58b567d
Sergey Potapov <posergyr@gmail.com>
2022-10-11 09:56:20 +0300

    Ladders and SM

--- Sprites/Player.gd
+++ Sprites/Player.gd
@@ -1,19 +1,36 @@
 extends KinematicBody2D
 class_name Player
 
+enum {
+	MOVE,
+	CLIMB,
+}
+
 export(Resource) var statsData
 
 var velocity = Vector2.ZERO
+var state = MOVE
+
 onready var animatedSprite = $AnimatedSprite
+onready var ladderCheck = $LadderCheck
 
 func _ready():
 	animatedSprite.frames = load("res://Resources/PlayerBlueSkin.tres")
 
 func _physics_process(_delta):
-	apply_gravity()
-	
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+	
+func move_state(input):
+	if is_on_ladder() and Input.is_action_pressed("ui_up"):
+		state = CLIMB
+	
+	apply_gravity()
 	
 	if input.x == 0:
 		apply_friction()
@@ -35,14 +52,31 @@ func _physics_process(_delta):
 	if velocity.y > 0:
 		velocity.y += statsData.ADDITONAL_GRAVITY
 
-	var was_in_air = !is_on_floor()
+	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
-	var just_landed = is_on_floor() && was_in_air
+	var just_landed = is_on_floor() and was_in_air
 	
 	if just_landed:
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
 	
+func climb_state(input):
+	if not is_on_ladder(): state = MOVE
+	
+	if input.length() != 0:
+		animatedSprite.animation = "Run"
+	else:
+		animatedSprite.animation = "Idle"
+	
+	velocity = input * 50
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+func is_on_ladder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
+
 func apply_gravity():
 	velocity.y += statsData.GRAVITY
 	velocity.y = min(velocity.y, 250)
@@ -52,4 +86,3 @@ func apply_friction():
 
 func apply_acceleration(amount):
 	velocity.x = move_toward(velocity.x, statsData.MAX_SPEED * amount, statsData.ACCELERATION)
-	
